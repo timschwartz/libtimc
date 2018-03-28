e87c30af218392e3f6d504fec9d91ac0362a429b
Tim Schwartz <tim@metaverse.systems>
2018-03-27 23:24:38 -0500

    Added 8 and 16 bit integer types

--- include/stdint.h
+++ include/stdint.h
@@ -1,6 +1,12 @@
 #ifndef STDINT_H
 #define STDINT_H
 
+typedef char int8_t;
+typedef unsigned char uint8_t;
+
+typedef short int16_t;
+typedef unsigned short uint16_t;
+
 typedef long int int64_t;
 typedef unsigned long int uint64_t;
 
